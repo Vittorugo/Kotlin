31f05fc0563773f54a53c64632a58e785332e292
80723323 <victtor.hugo.l.costa@accenture.com>
2022-05-31 17:39:57 -0300

    Adicionado autenticação via token JWT

--- ForumKotlin/src/main/kotlin/com/kotlin/forumkotlin/config/SecurityConfiguration.kt
+++ ForumKotlin/src/main/kotlin/com/kotlin/forumkotlin/config/SecurityConfiguration.kt
@@ -1,8 +1,11 @@
 package com.kotlin.forumkotlin.config
 
+import com.kotlin.forumkotlin.security.JWTAuthenticationFilter
+import com.kotlin.forumkotlin.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Lazy
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -10,26 +13,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.filter.OncePerRequestFilter
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfiguration(
-    private val userDetailsService: UserDetailsService
+    private val userDetailsService: UserDetailsService,
+    private val jwtUtil: JWTUtil
 ): WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
         http?.
+        csrf()?.disable()?.
         authorizeHttpRequests()?.
-        antMatchers("/topicos")?.
-        hasAuthority("LEITURA_ESCRITA")?.
+        antMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
+        antMatchers(HttpMethod.POST,"/login")?.permitAll()?.
         anyRequest()?.
         authenticated()?.
-        and()?.
-        sessionManagement()?.
-        sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.
-        and()?.
-        formLogin()?.disable()?.
-        httpBasic()
+        and()
+        http?.addFilterBefore(JWTLoginFilter(authManager = authenticationManager(), jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+        http?.addFilterBefore(JWTAuthenticationFilter(jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+        http?.sessionManagement()?.
+        sessionCreationPolicy(SessionCreationPolicy.STATELESS)
     }
 
     override fun configure(auth: AuthenticationManagerBuilder?) {
---
+++ ForumKotlin/src/main/kotlin/com/kotlin/forumkotlin/model/Credentials.kt
@@ -0,0 +1,6 @@
+package com.kotlin.forumkotlin.model
+
+data class Credentials(
+    val username: String = "",
+    val password: String = ""
+)
