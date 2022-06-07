5219340416da06080d0b0c656e6b067e436e2cda
80723323 <victtor.hugo.l.costa@accenture.com>
2022-06-06 23:32:18 -0300

    Adicionando consumidor Kafka com Kotlin

---
+++ KotlinEcommerceKafka/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+v
+Fsrc/main/kotlin/br/com/alura/ecommerce/service/FraudDetectorService.kt,a\b\aba1f8208bbc68cbc07f6ce7b76a06693ee9f5e6
+U
+%src/main/kotlin/EcommerceProductor.kt,1\0\108726c07a3abce8edaefbd6a0677b5944242d5f
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
--- KotlinEcommerceKafka/src/main/kotlin/EcommerceProductor.kt
+++ KotlinEcommerceKafka/src/main/kotlin/EcommerceProductor.kt
@@ -5,8 +5,8 @@ import java.util.*
 fun main(args: Array<String>) {
     println("Hello World, Kafka!")
 
-    val producer: KafkaProducer<String,String> = KafkaProducer<String, String>(properties())
-    val value = "123,456,789"
+    val producer: KafkaProducer<String,String> = KafkaProducer<String, String>(propertiesProducer())
+    val value = "123,444,321"
     val record: ProducerRecord<String, String> = ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value)
     producer.send(record) { data: RecordMetadata?, exception: Exception? ->
         exception?.printStackTrace()
@@ -14,7 +14,7 @@ fun main(args: Array<String>) {
     }.get()
 }
 
-fun properties() : Properties{
+fun propertiesProducer() : Properties{
     val properties = Properties()
     properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092")
     properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
